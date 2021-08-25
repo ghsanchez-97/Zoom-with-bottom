bb96918b9054474a059ab187d69db4e9dfb129dc
Guy Sanchez <anakin25@live.com>
2021-08-25 10:42:57 -0600

    :rocket: Otro proyecto con zoom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import PinchZoomPan from "react-responsive-pinch-zoom-pan";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: '500px', height: '500px'}}>
+      <PinchZoomPan>
+        <img alt="Test Image" src='https://images.unsplash.com/photo-1624471325375-995d4fdb877f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' />
+      </PinchZoomPan>
     </div>
   );
 }
