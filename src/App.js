e4a8af853f06a946b92205c11f8644ff86fae8a7
Guy Sanchez <anakin25@live.com>
2021-09-07 17:39:58 -0600

    :rocket: Finalización del proyecto

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import PinchZoomPan from "react-responsive-pinch-zoom-pan";
 function App() {
   return (
     <div style={{width: '500px', height: '500px'}}>
-      {/* <PinchZoomPan>
+      <PinchZoomPan>
         <img alt="Test Image" src='https://images.unsplash.com/photo-1624471325375-995d4fdb877f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' />
-      </PinchZoomPan> */}
+      </PinchZoomPan>
     </div>
   );
 }
